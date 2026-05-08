0e44ef8f8c7ab8863af0bb99c667280a4e720d9e
RootDev86 <root86@runtime.local>
2026-05-08 21:58:25 +0400

    restore clean architecture

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-Dictionary<string, string> runtime = new();
+using System;
 
 Console.Title = "ROOT86";
 
@@ -13,7 +13,6 @@ while (true)
     Console.Write(">_ ");
 
     string? input = Console.ReadLine();
-    input = input?.TrimStart();
 
     input = input?.Trim();
 
@@ -23,67 +22,5 @@ while (true)
         continue;
     }
 
-    // FILESYSTEM
-    if (input.StartsWith("fs.x01"))
-    {
-        FS.Read(input);
-        continue;
-    }
-
-    if (input.StartsWith("fs.x02"))
-    {
-        FS.Delete(input);
-        continue;
-    }
-
-    if (input.StartsWith("fs.x03"))
-    {
-        FS.Edit(input);
-        continue;
-    }
-
-    // =====================================
-    // LS NODE EXECUTION
-    // =====================================
-
-    if (input.StartsWith("ls."))
-    {
-        if (runtime.ContainsKey(input))
-        {
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-
-            Console.WriteLine(runtime[input]);
-
-            Console.ForegroundColor = ConsoleColor.White;
-        }
-
-        continue;
-    }
-
-    // RUNTIME NODES
-    if (input.Contains("="))
-    {
-        int index = input.IndexOf('=');
-
-        string node = input.Substring(0, index).Trim();
-        string value = input.Substring(index + 1).Trim();
-
-        runtime[node] = value;
-
-        Console.ForegroundColor = ConsoleColor.DarkGray;
-        Console.WriteLine($"[{node}]");
-        Console.ForegroundColor = ConsoleColor.White;
-
-        continue;
-    }
-
-    // EXECUTE NODE
-    if (runtime.ContainsKey(input))
-    {
-        Console.ForegroundColor = ConsoleColor.DarkGray;
-        Console.WriteLine(runtime[input]);
-        Console.ForegroundColor = ConsoleColor.White;
-
-        continue;
-    }
+    Parser.Parse(input);
 }
